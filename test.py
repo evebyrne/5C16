d50e9188378f16695fe05d7eef03f4f3c9f71fd3
byrnee39 <byrnee39@tcd.4c16>
2019-10-09 16:32:28 +0000

    initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+def say_stuff(): # defining a function
+    print("some day") # four spaces at the start
+    print("when the world is cold")
+    print("i will feel a glow")
+    print("just thinking of you")
+    print("and the way you look tonight")
+    print("hello")
